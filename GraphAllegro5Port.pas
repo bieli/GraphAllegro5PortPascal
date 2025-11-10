c142124a73fbe5380e993d16aa7fd050dc69c7e9
Marcin Bielak <marcin.bieli@gmail.com>
2025-11-10 10:15:55 +0100

    Add required implementations GraphAllegro5Port.pas

--- GraphAllegro5Port.pas
+++ GraphAllegro5Port.pas
@@ -12,9 +12,6 @@ Additional materials:
  - Allegro5 sources: https://github.com/liballeg/allegro5
  - Allegro5 Platform-specific functions: https://liballeg.org/a5docs/trunk/platform.html
  - Allegro5 addons: https://liballeg.org/a5docs/trunk/index.html#addons
-
-@Author: Marcin Bielak
-@Source: https://github.com/bieli/GraphAllegro5PortPascal
 *}
 
 interface
@@ -171,18 +168,47 @@ procedure CloseGraph;
 function GraphResult: Integer;
 
 function GraphErrorMsg(ErrCode: Integer): String;
+
+procedure SetColor(Color: Integer);
+//procedure SetFillStyle(Style: TFillStyle);
+procedure SetFillStyle(Pattern: Word; Color: TColorType);
+procedure SetLineStyle(LineStyle, Pattern, LineWidth: Word);
+procedure SetBkColor(Color: TColorType);
+
+procedure Line(X1, Y1, X2, Y2: Integer);
+procedure Rectangle(X1, Y1, X2, Y2: Integer);
+procedure Bar(X1, Y1, X2, Y2: Integer);
+procedure Circle(X, Y, Radius: Integer);
+procedure Ellipse(X, Y, XRadius, YRadius: Integer);
+procedure FillEllipse(X, Y, XRadius, YRadius: Integer);
+procedure DrawPoly(NumPoints: Integer; Points: array of Integer);
+procedure FillPoly(NumPoints: Integer; Points: array of Integer);
+procedure OutTextXY(X, Y: Integer; Text: String);
+
+function RegisterBGIFont(const FontPath: string; Size: Integer): Pointer;
+procedure SetTextStyle(Font: Pointer; Direction, Size: Integer);
+
 procedure ClearDevice;
 
-procedure SetBkColor(Color: TColorType);
+procedure PieSlice(X, Y, StAngle, EndAngle, Radius: Integer);
+procedure Bar3D(X1, Y1, X2, Y2, Depth: Integer; TopFlag: Boolean);
+procedure Sector(X, Y, StAngle, EndAngle, XRadius, YRadius: Integer);
+
+procedure MoveTo(X, Y: Integer);
+procedure LineTo(X, Y: Integer);
+procedure MoveRel(DX, DY: Integer);
+procedure LineRel(DX, DY: Integer);
 
 function GetMaxX: Integer;
 function GetMaxY: Integer;
 
-procedure OutTextXY(X, Y: Integer; Text: String);
-function RegisterBGIFont(const FontPath: string; Size: Integer): Pointer;
-procedure SetTextStyle(Font: Pointer; Direction, Size: Integer);
 
 procedure FlipDisplay;
+
+function GetScreenDimensions: Boolean;
+procedure CenterWindowOnScreen(DisplayWidth, DisplayHeight: Integer);
+procedure ResizeFont(FontPath: AnsiString; NewSize: Integer);
+
 
 implementation
 
@@ -305,7 +331,7 @@ begin
   Result := LastErrorCode;
 end;
 
-function GraphErrorMsg(EGraphErrorMsgrrCode: Integer): String;
+function GraphErrorMsg(ErrCode: Integer): String;
 begin
   if (ErrCode >= Low(ErrorMessages)) and (ErrCode <= High(ErrorMessages)) then
     Result := ErrorMessages[ErrCode]
@@ -313,10 +339,41 @@ begin
     Result := 'Unknown error code.';
 end;
 
-procedure ClearDevice;
+procedure SetColor(Color: Integer);
 begin
-  al_clear_to_color(al_map_rgb(0, 0, 0));
-  //al_flip_display;
+  case Color of
+    Black:       CurrentColor := al_map_rgb(0, 0, 0);
+    Blue:        CurrentColor := al_map_rgb(0, 0, 255);
+    Green:       CurrentColor := al_map_rgb(0, 255, 0);
+    Cyan:        CurrentColor := al_map_rgb(0, 255, 255);
+    Red:         CurrentColor := al_map_rgb(255, 0, 0);
+    Magenta:     CurrentColor := al_map_rgb(255, 0, 255);
+    Brown:       CurrentColor := al_map_rgb(165, 42, 42);
+    LightGray:   CurrentColor := al_map_rgb(211, 211, 211);
+    DarkGray:    CurrentColor := al_map_rgb(169, 169, 169);
+    LightBlue:   CurrentColor := al_map_rgb(173, 216, 230);
+    LightGreen:  CurrentColor := al_map_rgb(144, 238, 144);
+    LightCyan:   CurrentColor := al_map_rgb(224, 255, 255);
+    LightRed:    CurrentColor := al_map_rgb(255, 182, 193);
+    LightMagenta:CurrentColor := al_map_rgb(255, 160, 122);
+    Yellow:      CurrentColor := al_map_rgb(255, 255, 0);
+    White:       CurrentColor := al_map_rgb(255, 255, 255);
+  else
+    CurrentColor := al_map_rgb(255, 255, 255);
+  end;
+end;
+
+//procedure SetFillStyle(Style: TFillStyle);
+procedure SetFillStyle(Pattern: Word; Color: TColorType);
+begin
+  //CurrentFillStyle := Style;
+  CurrentFillStylePattern := Pattern;
+  SetColor(Color);
+end;
+
+procedure SetLineStyle(LineStyle, Pattern, LineWidth: Word);
+begin
+  CurrentLineWidth := LineWidth;
 end;
 
 procedure SetBkColor(Color: TColorType);
@@ -326,20 +383,74 @@ begin
   //al_flip_display;
 end;
 
-function GetMaxX: Integer;
+procedure Line(X1, Y1, X2, Y2: Integer);
 begin
-  if Display <> nil then
-    Result := al_get_display_width(Display)
-  else
-    Result := -1;  // Return -1 if the display is not initialized
+  al_draw_line(X1, Y1, X2, Y2, CurrentColor, CurrentLineWidth);
+  //al_flip_display;
 end;
 
-function GetMaxY: Integer;
+procedure Rectangle(X1, Y1, X2, Y2: Integer);
 begin
-  if Display <> nil then
-    Result := al_get_display_height(Display)
-  else
-    Result := -1;  // Return -1 if the display is not initialized
+  al_draw_rectangle(X1, Y1, X2, Y2, CurrentColor, CurrentLineWidth);
+  //al_flip_display;
+end;
+
+procedure Bar(X1, Y1, X2, Y2: Integer);
+begin
+  al_draw_filled_rectangle(X1, Y1, X2, Y2, CurrentColor);
+  //al_flip_display;
+end;
+
+procedure Circle(X, Y, Radius: Integer);
+begin
+  al_draw_circle(X, Y, Radius, CurrentColor, CurrentLineWidth);
+  //al_flip_display;
+end;
+
+procedure Ellipse(X, Y, XRadius, YRadius: Integer);
+begin
+  al_draw_ellipse(X, Y, XRadius, YRadius, CurrentColor, CurrentLineWidth);
+  //al_flip_display;
+end;
+
+procedure FillEllipse(X, Y, XRadius, YRadius: Integer);
+begin
+  al_draw_filled_ellipse(X, Y, XRadius, YRadius, CurrentColor);
+  //al_flip_display;
+end;
+
+procedure DrawPoly(NumPoints: Integer; Points: array of Integer);
+var
+  i: Integer;
+  Vertices: array of ALLEGRO_VERTEX;
+begin
+  SetLength(Vertices, NumPoints);
+  for i := 0 to NumPoints - 1 do
+  begin
+    Vertices[i].x := Points[2 * i];
+    Vertices[i].y := Points[2 * i + 1];
+    Vertices[i].z := 0;
+    Vertices[i].color := CurrentColor;
+  end;
+  al_draw_prim(Vertices, nil, 0, NumPoints, ALLEGRO_PRIM_LINE_LOOP);
+  //al_flip_display;
+end;
+
+procedure FillPoly(NumPoints: Integer; Points: array of Integer);
+var
+  i: Integer;
+  Vertices: array of ALLEGRO_VERTEX;
+begin
+  SetLength(Vertices, NumPoints);
+  for i := 0 to NumPoints - 1 do
+  begin
+    Vertices[i].x := Points[2 * i];
+    Vertices[i].y := Points[2 * i + 1];
+    Vertices[i].z := 0;
+    Vertices[i].color := CurrentColor;
+  end;
+  al_draw_prim(Vertices, nil, 0, NumPoints, ALLEGRO_PRIM_TRIANGLE_FAN);
+  //al_flip_display;
 end;
 
 procedure OutTextXY(X, Y: Integer; Text: String);
@@ -365,6 +476,85 @@ begin
   // Allegro doesn't use SetTextStyle directly — you rotate text manually if needed
 end;
 
+procedure ClearDevice;
+begin
+  al_clear_to_color(al_map_rgb(0, 0, 0));
+  //al_flip_display;
+end;
+
+procedure PieSlice(X, Y, StAngle, EndAngle, Radius: Integer);
+begin
+  al_draw_pieslice(X, Y, Radius, StAngle * ALLEGRO_PI / 180, (EndAngle - StAngle) * ALLEGRO_PI / 180, CurrentColor, CurrentLineWidth);
+  //al_flip_display;
+end;
+
+procedure Bar3D(X1, Y1, X2, Y2, Depth: Integer; TopFlag: Boolean);
+begin
+  al_draw_filled_rectangle(X1, Y1, X2, Y2, CurrentColor);
+  al_draw_filled_rectangle(X1, Y1 - Depth, X2, Y2 - Depth, CurrentColor);
+  al_draw_line(X1, Y1, X1, Y1 - Depth, CurrentColor, CurrentLineWidth);
+  al_draw_line(X2, Y1, X2, Y1 - Depth, CurrentColor, CurrentLineWidth);
+  al_draw_line(X1, Y2, X1, Y2 - Depth, CurrentColor, CurrentLineWidth);
+  al_draw_line(X2, Y2, X2, Y2 - Depth, CurrentColor, CurrentLineWidth);
+  if TopFlag then
+    al_draw_filled_rectangle(X1, Y1 - Depth, X2, Y2 - Depth, CurrentColor);
+  //al_flip_display;
+end;
+
+procedure Sector(X, Y, StAngle, EndAngle, XRadius, YRadius: Integer);
+begin
+  al_draw_filled_pieslice(X, Y, XRadius, StAngle * ALLEGRO_PI / 180, (EndAngle - StAngle) * ALLEGRO_PI / 180, CurrentColor);
+  //al_flip_display;
+end;
+
+procedure MoveTo(X, Y: Integer);
+begin
+  CurrentX := X;
+  CurrentY := Y;
+end;
+
+procedure LineTo(X, Y: Integer);
+begin
+  al_draw_line(CurrentX, CurrentY, X, Y, CurrentColor, CurrentLineWidth);
+  //al_flip_display;
+  CurrentX := X;
+  CurrentY := Y;
+end;
+
+procedure MoveRel(DX, DY: Integer);
+begin
+  CurrentX := CurrentX + DX;
+  CurrentY := CurrentY + DY;
+end;
+
+procedure LineRel(DX, DY: Integer);
+var
+  NewX, NewY: Integer;
+begin
+  NewX := CurrentX + DX;
+  NewY := CurrentY + DY;
+  al_draw_line(CurrentX, CurrentY, NewX, NewY, CurrentColor, CurrentLineWidth);
+  //al_flip_display;
+  CurrentX := NewX;
+  CurrentY := NewY;
+end;
+
+function GetMaxX: Integer;
+begin
+  if Display <> nil then
+    Result := al_get_display_width(Display)
+  else
+    Result := -1;  // Return -1 if the display is not initialized
+end;
+
+function GetMaxY: Integer;
+begin
+  if Display <> nil then
+    Result := al_get_display_height(Display)
+  else
+    Result := -1;  // Return -1 if the display is not initialized
+end;
+
 {*
 ----- OUTSIDE GRAPH LIBRARY - ALLEGRO5 SPECIFIC
 *}
@@ -372,6 +562,89 @@ end;
 procedure FlipDisplay();
 begin
   al_flip_display();
+end;
+
+function GetScreenDimensions(): Boolean;
+var
+  MonitorsCount: Integer;
+  MonitorIndex: Integer;
+  MonitorIndexSelected: Integer;
+  MonitorInfo: ALLEGRO_MONITOR_INFO;
+begin
+  MonitorsCount := al_get_num_video_adapters();
+
+  if MonitorsCount = 0 then
+  begin
+    LastErrorCode := 7; // No monitors detected
+    raise Exception.Create(GraphErrorMsg(LastErrorCode));
+  end;
+
+  {Writeln('Number of monitors detected: ', MonitorsCount);}
+  
+  for MonitorIndex := 0 to MonitorsCount - 1 do
+  begin
+    if al_get_monitor_info(MonitorIndex, MonitorInfo) then
+    begin
+      MonitorIndexSelected := MonitorIndex;
+      {Writeln('[INFO] Monitor ', MonitorIndex, ':');}
+      {Writeln('[INFO]   Position: (', MonitorInfo.x1, ', ', MonitorInfo.y1, ') - (', MonitorInfo.x2, ', ', MonitorInfo.y2, ')');}
+    end
+    else
+    begin
+      Writeln('[ERROR] Failed to get info for monitor ', MonitorIndex);
+      Halt(1);
+    end;
+  end;
+
+  if MonitorIndexSelected >= MonitorsCount then
+  begin
+    Writeln('[ERROR] Invalid monitor index!');
+    Halt(1);
+  end;
+
+  al_set_new_display_adapter(MonitorIndexSelected);
+
+  if al_get_monitor_info(MonitorIndexSelected, MonitorInfo) then
+  begin
+    ScreenWidth := MonitorInfo.x2 - MonitorInfo.x1;
+    ScreenHeight := MonitorInfo.y2 - MonitorInfo.y1;
+    Result := True;
+  end
+  else
+  begin
+    Result := False;
+  end;
+end;
+
+procedure CenterWindowOnScreen(DisplayWidth, DisplayHeight: Integer);
+begin  
+  if GetScreenDimensions then
+  begin
+    al_set_new_display_flags(ALLEGRO_WINDOWED);
+    Display := al_create_display(DisplayWidth, DisplayHeight);
+    if Display = nil then
+    begin
+      LastErrorCode := 5; // Error creating display
+      raise Exception.Create(GraphErrorMsg(LastErrorCode));
+    end;
+
+    al_set_window_position(Display, (ScreenWidth - DisplayWidth) div 2, (ScreenHeight - DisplayHeight) div 2);
+  end
+  else
+  begin
+    raise Exception.Create('Failed to get screen dimensions');
+  end;
+end;
+
+procedure ResizeFont(FontPath: AnsiString; NewSize: Integer);
+begin
+  {Writeln('[DEBUG] FontPath:', FontPath);}
+  Font := al_load_font(FontPath, NewSize, 0);
+  if Font = nil then
+    begin
+      LastErrorCode := 8; // Failed to resize font
+      raise Exception.Create(GraphErrorMsg(LastErrorCode));
+    end;
 end;
 
 end.
