846aaaf25bad4736be8e49fd9654a37b54410821
Marcin Bielak <marcin.bieli@gmail.com>
2025-11-10 10:55:35 +0100

    Add animations and real primitives demo

--- example.pas
+++ example.pas
@@ -2,73 +2,207 @@ program GraphExample;
 
 {$mode objfpc}{$H+}
 
-{*
-One-line compile command:
-$ fpc -Fu"${HOME}/fpcupdeluxe/ccr/allegro-pas/src/lib/" example.pas
-*}
-
-Uses Crt,
-{$if FPC_fullVersion >= 20701}
-GraphAllegro5Port
-{$else}
-Graph
-{$endif}
+uses
+  Crt,
+  {$if FPC_fullVersion >= 20701}
+  GraphAllegro5Port
+  {$else}
+  Graph
+  {$endif}
 ;
 
 const
   FontPath = '/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf';
+  FrameDelay = 30;
+  MaxLines = 50;
+  RobotSpeed = 2;
+  LaserLength = 180;
 
 var
   GraphicsDriver, GraphicsMode, ErrCode: Integer;
-  MidX, MidY, HorizDir: Integer;
   FontHandle: Pointer;
+  MaxX, MaxY, MidX, MidY: Integer;
+  Angle: Integer;
+  BallX, BallY, BallDX, BallDY: Integer;
+  RobotX, RobotY: Integer;
+  LaserActive: Boolean;
+  LaserTimer: Integer;
+  PolyPoints: array[0..7] of Integer;
+  LineData: array[0..MaxLines - 1] of record
+    x1, y1, x2, y2: Integer;
+    color: Integer;
+  end;
 
+procedure Init;
+var
+  i: Integer;
 begin
+  GraphicsDriver := Detect();
   {$if FPC_fullVersion >= 20701}
-    GraphicsMode := m640x480;
+    GraphicsMode := m800x600;
   {$else}
     GraphicsMode := 0;
   {$endif}
-
-  GraphicsDriver := Detect();
-  InitGraph(GraphicsDriver, GraphicsMode, 'c:\tp70\bgi');
+  InitGraph(GraphicsDriver, GraphicsMode, '');
   ErrCode := GraphResult();
-  If ErrCode <> grOK then
-  Begin
-    ClrScr();
-    Writeln('Graphics error occured: ', GraphErrorMsg(ErrCode));
-    Writeln('If a file not found error is displayed above');
-    Writeln('then, change the dir from the current');
-    Writeln('location to C:\ -> TP -> BGI, from the file menu!');
-    Readln;
+  if ErrCode <> grOK then
+  begin
+    ClrScr;
+    Writeln('Graphics error: ', GraphErrorMsg(ErrCode));
+    ReadLn;
     Halt(1);
-  End;
+  end;
 
-  ClearDevice();
+  MaxX := GetMaxX;
+  MaxY := GetMaxY;
+  MidX := MaxX div 2;
+  MidY := MaxY div 2;
 
-  SetBkColor(Blue);
+  SetBkColor(DarkGray);
+  ClearDevice;
 
-
-  MidX := GetMaxX() div 6;
-  MidY := GetMaxY() div 2;
-  SetColor(White);
-  HorizDir := 1;
-  
   {$if FPC_fullVersion >= 20701}
-    FontHandle := RegisterBGIFont(FontPath, 22);
-    SetTextStyle(FontHandle, HorizDir, 22);
+    FontHandle := RegisterBGIFont(FontPath, 20);
   {$else}
     FontHandle := RegisterBGIFont('GOTH.CHR');
-    SetTextStyle(FontHandle, HorizDir, 22);
   {$endif}
-  
-  OutTextXY(MidX, MidY, 'Hello, from GraphAllegro5Port example...');
+  SetTextStyle(FontHandle, 0, 20);
 
-  // NOT SPECIFIC FOR 'Graph' LIBRARY, BUT REQUIRED TO CALLING IN THIS PORT!
-  {$if FPC_fullVersion >= 20701}
-  FlipDisplay();
-  {$endif}
+  BallX := MidX;
+  BallY := MidY;
+  BallDX := 4;
+  BallDY := 3;
 
-  ReadLn; // Wait for user input before closing
-  CloseGraph();
+  RobotX := 50;
+  RobotY := MaxY - 350;
+  LaserActive := False;
+  LaserTimer := 0;
+
+  Randomize;
+  for i := 0 to MaxLines - 1 do
+  begin
+    LineData[i].x1 := Random(MaxX);
+    LineData[i].y1 := Random(MaxY);
+    LineData[i].x2 := Random(MaxX);
+    LineData[i].y2 := Random(MaxY);
+    LineData[i].color := Random(15) + 1;
+  end;
+end;
+
+procedure DrawStaticPrimitives;
+begin
+  SetColor(Yellow);
+  Line(20, 20, MaxX - 20, 20);
+  Rectangle(50, 50, 150, 100);
+  Bar(160, 50, 260, 100);
+  SetFillStyle(1, LightBlue);
+  FillEllipse(320, 75, 40, 25);
+  Circle(400, 75, 30);
+  Ellipse(480, 75, 60, 30);
+  PieSlice(560, 75, 30, 300, 40);
+  Bar3D(620, 50, 700, 100, 15, True);
+  Sector(760, 75, 0, 180, 40, 25);
+
+  SetColor(LightGreen);
+  PolyPoints[0] := 100; PolyPoints[1] := 200;
+  PolyPoints[2] := 150; PolyPoints[3] := 250;
+  PolyPoints[4] := 100; PolyPoints[5] := 300;
+  PolyPoints[6] :=  50; PolyPoints[7] := 250;
+  DrawPoly(4, PolyPoints);
+  SetFillStyle(2, Green);
+  FillPoly(4, PolyPoints);
+
+  MoveTo(200, 200);
+  LineTo(300, 200);
+  MoveRel(0, 20);
+  LineRel(100, 50);
+
+  SetColor(White);
+  OutTextXY(50, MaxY - 60, 'GraphAllegro5Port Demo');
+end;
+
+procedure DrawRobot(X, Y: Integer);
+begin
+  SetColor(Yellow);
+  SetFillStyle(1, Yellow);
+  Bar(X, Y, X + 30, Y + 40);         // Body
+  Circle(X + 15, Y - 10, 10);        // Head
+  Line(X, Y + 10, X - 10, Y + 20);   // Left arm
+  Line(X + 30, Y + 10, X + 40, Y + 20); // Right arm
+  Line(X + 10, Y + 40, X + 10, Y + 60); // Left leg
+  Line(X + 20, Y + 40, X + 20, Y + 60); // Right leg
+
+  if LaserActive then
+  begin
+    SetColor(Red);
+    Line(X + 40, Y + 15, X + 40 + LaserLength, Y + 15);
+  end;
+end;
+
+procedure DrawAnimatedOverlay;
+var
+  i: Integer;
+begin
+  for i := 0 to MaxLines - 1 do
+  begin
+    SetColor(LineData[i].color);
+    Line(LineData[i].x1, LineData[i].y1, LineData[i].x2, LineData[i].y2);
+    Inc(LineData[i].x1, Random(5) - 1);
+    Inc(LineData[i].y1, Random(5) - 1);
+    Inc(LineData[i].x2, Random(5) - 1);
+    Inc(LineData[i].y2, Random(5) - 1);
+  end;
+
+  SetFillStyle(5, Cyan);
+  SetColor(Cyan);
+  Line(20 + Angle, 20, MaxX - 20 - Angle, 20);
+
+  SetColor(Red);
+  Circle(BallX, BallY, 20);
+  SetFillStyle(1, Red);
+  FillEllipse(BallX, BallY, 20, 20);
+
+  SetColor(LightRed);
+  PieSlice(MidX, MaxY - 200, Angle, Angle + 90, 100);
+
+  DrawRobot(RobotX, RobotY);
+end;
+
+procedure UpdateState;
+begin
+  Inc(Angle, 5);
+  if Angle >= 360 then Angle := 0;
+
+  Inc(BallX, BallDX);
+  Inc(BallY, BallDY);
+  if (BallX < 20) or (BallX > MaxX - 20) then BallDX := -BallDX;
+  if (BallY < 20) or (BallY > MaxY - 20) then BallDY := -BallDY;
+
+  Inc(RobotX, RobotSpeed);
+  if RobotX > MaxX then RobotX := -50;
+
+  Inc(LaserTimer);
+  if LaserTimer mod 60 < 20 then
+    LaserActive := True
+  else
+    LaserActive := False;
+end;
+
+begin
+  Init;
+  Angle := 0;
+  //Delay(15000);
+
+  repeat
+    ClearDevice;
+    DrawStaticPrimitives;
+    DrawAnimatedOverlay;
+    UpdateState;
+    {$if FPC_fullVersion >= 20701}
+    FlipDisplay;
+    {$endif}
+    Delay(FrameDelay);
+  until KeyPressed;
+
+  CloseGraph;
 end.
